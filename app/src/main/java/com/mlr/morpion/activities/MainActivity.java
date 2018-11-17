b590f5376b6417d4ce4b2c86ec3b89c40ebfe772
mlr46 <mlr46@users.noreply.github.com>
2018-11-17 16:46:06 -0500

    Main activity screen

---
+++ app/src/main/java/com/mlr/morpion/activities/MainActivity.java
@@ -0,0 +1,20 @@
+package com.mlr.morpion.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.mlr.morpion.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button startBtn;
+    private TextView gridSize;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
