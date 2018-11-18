86d261a18bb72847c4d46085c5903dc5f8c3b91a
mlr46 <mlr46@users.noreply.github.com>
2018-11-17 21:23:30 -0500

    Adding immutable library and a bit of cleanup

--- app/src/main/java/com/mlr/morpion/activities/MainActivity.java
+++ app/src/main/java/com/mlr/morpion/activities/MainActivity.java
@@ -3,6 +3,7 @@ package com.mlr.morpion.activities;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 import com.mlr.morpion.R;
@@ -10,7 +11,8 @@ import com.mlr.morpion.R;
 public class MainActivity extends AppCompatActivity {
 
     private Button startBtn;
-    private TextView gridSize;
+    private TextView tvGridSize;
+    private SeekBar gridSize;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ app/src/main/java/com/mlr/morpion/views/Grid.java
@@ -0,0 +1,26 @@
+package com.mlr.morpion.views;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class Grid extends View {
+
+    private int size;
+
+    public Grid(Context context) {
+        super(context);
+    }
+
+    public Grid(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public Grid(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    private void init() {
+
+    }
+}
